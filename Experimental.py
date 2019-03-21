fe4c337607ea87bbbaed7d036d62d8a58d3a6d41
ten18 <48300794+ten18@users.noreply.github.com>
2019-03-21 14:12:35 -0400

    Add files via upload

--- Experimental.py
+++ Experimental.py
@@ -14,9 +14,29 @@ def Main(undetectedAttackSuccess, detectedAttackSuccess, costOfSignalling,
             #print("We'll repeat the simulation " + str(n) + " time(s).")
             break
 
+    residentPayoffList = []
+    mutantPayoffList = []
+    endSituationList = []
+
     for x in range(n):
         #print("run")
-        GI.predPrey(undetectedAttackSuccess, detectedAttackSuccess, costOfSignalling,
+        residentPayoff, mutantPayoff, endSituation = GI.predPrey(undetectedAttackSuccess, detectedAttackSuccess, costOfSignalling,
         residentSignalChance, mutantSignalChance, detectionProbability, attackProbablility)
+        residentPayoffList.append(residentPayoff)
+        mutantPayoffList.append(mutantPayoff)
+        endSituationList.append(endSituation)
+    print("Total resident payoff: " , sum(residentPayoffList))
+    print("Total mutant payoff: " , sum(mutantPayoffList))
+
+
+
+
+
+
+
+
+
+
+
 
 Main(60,30,20,30,50,50,50)
