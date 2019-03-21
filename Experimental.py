0809a63730f2adef5feb5934f3d2daa0833a5c2e
pav17 <pav17@hampshire.edu>
2019-03-21 14:22:09 -0400

    started data analysis

--- Experimental.py
+++ Experimental.py
@@ -25,18 +25,26 @@ def Main(undetectedAttackSuccess, detectedAttackSuccess, costOfSignalling,
         residentPayoffList.append(residentPayoff)
         mutantPayoffList.append(mutantPayoff)
         endSituationList.append(endSituation)
-    print("Total resident payoff: " , sum(residentPayoffList))
-    print("Total mutant payoff: " , sum(mutantPayoffList))
 
+    print("Total resident payoff: " , sum(residentPayoffList), "for an average payoff of: ", sum(residentPayoffList)/n)
+    print("Total mutant payoff: " , sum(mutantPayoffList), "for an average payoff of: ", sum(mutantPayoffList)/n)
 
+    situation1Count = endSituationList.count(1)
+    situation2Count = endSituationList.count(2)
+    situation3Count = endSituationList.count(3)
+    situation4Count = endSituationList.count(4)
+    situation5Count = endSituationList.count(5)
+    situation6Count = endSituationList.count(6)
+    situation7Count = endSituationList.count(7)
+    situation8Count = endSituationList.count(8)
+    situation9Count = endSituationList.count(9)
+    situation10Count = endSituationList.count(10)
+    situation11Count = endSituationList.count(11)
+    situation12Count = endSituationList.count(12)
+    situation13Count = endSituationList.count(13)
+    situation14Count = endSituationList.count(14)
 
-
-
-
-
-
-
-
+    print("Both detected, both signaled, and the predator attacked", situation1Count, "times.")
 
 
 Main(60,30,20,30,50,50,50)
